f12ac30990a09da8d4124e8a91cdf34b3bda0f9b
Tilo <Tilo-K@users.noreply.github.com>
2024-09-07 23:38:44 +0200

    Dev (#2)
    * feature list commad (#1)
    
    * Can do basic listing of files.
    
    * Added hours and minutes to output.
    
    * Now checks if user has access to metadata
    
    * added unit tests
    
    * Changed the dir arg so it doesn't require the prefix
    
    * Feature: Started the copy subcommand, not yet functional
    
    * single file copy is working now
    
    * Basic copying of multiple files
    
    * Added support for folder copying
    
    * Added delete function
    
    * README
    
    * feat: add github actions
    
    * feat: add cleanup readme
    
    * chore: adjust ci name

---
+++ README.md
@@ -0,0 +1,27 @@
+# RFiles
+
+RFiles is a simple CLI Utility to manage files.
+
+|             | Build                                 | Test                                |
+| ----------- | ------------------------------------- | ----------------------------------- |
+| **Linux**   | ![linux build][linux_build_badge]     | ![linux test][linux_test_badge]     |
+| **Windows** | ![windows build][windows_build_badge] | ![windows test][windows_test_badge] |
+| **MacOS**   | ![macos build][macos_build_badge]     | ![macos test][macos_test_badge]     |
+
+<!-- START BADGES -->
+
+<!-- linux -->
+[linux_build_badge]: https://img.shields.io/github/workflow/status/Tilo-K/rfiles/build_linux/main?logo=linux&logoColor=white&style=flat-square
+[linux_test_badge]: https://img.shields.io/github/workflow/status/Tilo-K/rfiles/test_linux/main?logo=linux&logoColor=white&style=flat-square&label=test
+[format_badge]: https://img.shields.io/github/workflow/status/Tilo-K/rfiles/format/main?logo=linux&logoColor=white&style=flat-square&label=format
+[coverage_badge]: https://img.shields.io/codecov/c/github/Tilo-K/rfiles/main?style=flat-square&token=71P6IZY43W&logo=linux&logoColor=white
+
+<!-- windows -->
+[windows_build_badge]: https://img.shields.io/github/workflow/status/Tilo-K/rfiles/build_windows/main?logo=windows&logoColor=white&style=flat-square
+[windows_test_badge]: https://img.shields.io/github/workflow/status/Tilo-K/rfiles/test_windows/main?logo=windows&logoColor=white&style=flat-square&label=test
+
+<!-- macos -->
+[macos_build_badge]: https://img.shields.io/github/workflow/status/Tilo-K/rfiles/build_macos/main?logo=apple&logoColor=white&style=flat-square
+[macos_test_badge]: https://img.shields.io/github/workflow/status/Tilo-K/rfiles/test_macos/main?logo=apple&logoColor=white&style=flat-square&label=test
+
+<!-- END BADGES -->
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+extern crate chrono;
+use chrono::offset::Local;
+use chrono::DateTime;
 use clap::{Parser, Subcommand};
+use glob::{glob, GlobError};
+use std::{
+    fs::{self, File},
+    io::{self, Read, Write},
+    path::PathBuf,
+};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about= None)]
@@ -10,16 +19,206 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     List {
-        #[arg(short, long)]
         dir: String,
 
-        #[arg(short, long)]
-        recursive: Option<bool>,
+        #[clap(short, long)]
+        recursive: bool,
+    },
+    Copy {
+        source: String,
+        target: String,
+    },
+    Delete {
+        target: String,
     },
 }
 
-fn list_dir(dir: String, recursvie: Option<bool>) {
-    println!("{}", dir);
+fn get_dir(dir: &String, recursive: bool, depth: u16) -> Vec<String> {
+    let md = fs::metadata(dir).unwrap();
+    if !md.is_dir() || depth > 100 {
+        return vec![];
+    }
+
+    let mut result = vec![];
+    let paths = fs::read_dir(dir).unwrap();
+
+    for path in paths {
+        let path = path.unwrap().path().display().to_string();
+        let meta = fs::metadata(path.clone());
+        let entry: String;
+        let mut is_dir = false;
+
+        match meta {
+            Ok(data) => {
+                let datetime: DateTime<Local> = data.accessed().unwrap().into();
+                entry = format!("{}\t{}", datetime.format("%d.%m.%Y %k:%M"), &path);
+                is_dir = data.is_dir()
+            }
+            _ => {
+                entry = format!("No access\t\t{}", &path);
+            }
+        }
+        result.push(entry);
+
+        if is_dir && recursive {
+            let mut sub_dir = get_dir(&path, recursive, depth + 1);
+            result.append(&mut sub_dir);
+        }
+    }
+
+    return result;
+}
+
+fn list_dir(dir: String, recursive: bool) {
+    let list: Vec<String> = get_dir(&dir, recursive, 0);
+
+    let mut output = format!("Listing {}\nLast accesed\t\tFilepath\n\n", &dir);
+    for file in list {
+        output = format!("{}{}\n", output, file);
+    }
+
+    println!("{}", output);
+}
+
+fn copy_folder(src: &PathBuf, dst: &PathBuf) -> Result<usize, io::Error> {
+    let mut written: usize = 0;
+    let folder_name = src.file_name().expect("Invalid source folder");
+    let dst_folder = dst.join(folder_name);
+
+    fs::create_dir_all(&dst_folder).expect("Couldn't create targer folder");
+
+    let files = fs::read_dir(src).expect("No access to source folder");
+
+    for entry in files {
+        if entry.is_err() {
+            continue;
+        }
+
+        let file = entry.unwrap();
+        let meta = match file.metadata() {
+            Ok(d) => d,
+            Err(_) => continue,
+        };
+
+        if meta.is_dir() {
+            //TODO: Probably should take out the recursion using a queue ors smth
+            written += copy_folder(&file.path(), &dst_folder)?;
+        } else if meta.is_file() {
+            written += copy_file(&file.path(), &dst_folder.join(file.file_name()))?;
+        }
+    }
+
+    Ok(written)
+}
+
+fn copy_file(src: &PathBuf, dst: &PathBuf) -> Result<usize, io::Error> {
+    let src_file = File::open(src)?;
+    let mut dst_file = File::create(dst)?;
+    let mut written: usize = 0;
+    let mut src_bytes = src_file.bytes();
+
+    let mut b = src_bytes.next();
+
+    while let Some(buff) = b {
+        written += dst_file
+            .write(&buff.expect("Error reading").to_ne_bytes())
+            .expect("Error writing");
+        b = src_bytes.next();
+    }
+
+    Ok(written)
+}
+
+fn copy(source: String, target: String) {
+    let files = glob(&source).expect("Invalid source");
+    let target_glob = glob(&target).expect("Invalid target");
+    let source_paths: Vec<Result<PathBuf, GlobError>> = files.collect();
+    let target_paths: Vec<Result<PathBuf, GlobError>> = target_glob.collect();
+
+    if target_paths.len() > 1 {
+        println!("Target must be a single file or directory !");
+    }
+
+    let target_path: &PathBuf = &PathBuf::from(target);
+    if target_paths.len() == 1 {
+        let target_path = target_paths
+            .first()
+            .expect("No target !")
+            .as_ref()
+            .expect("Invalid target");
+        let target_metadata = fs::metadata(&target_path).expect("No access to target !");
+
+        if target_metadata.is_file() && source_paths.len() > 1 {
+            println!("Can't copy multiple files into a single file");
+        }
+    }
+    if source_paths.len() == 1 {
+        let src_file = source_paths
+            .first()
+            .expect("Invalid source file !")
+            .as_ref()
+            .expect("Invalid Path to source file !");
+
+        let meta = fs::metadata(src_file).expect("No access to file !");
+
+        if meta.is_file() {
+            copy_file(&src_file, &target_path).expect("Error copying file !");
+        } else if meta.is_dir() {
+            copy_folder(&src_file, &target_path).expect("Error copying folder");
+        }
+    } else if source_paths.len() > 1 {
+        //TODO: Make this more performant
+        for src_file in source_paths.iter() {
+            if !src_file.is_ok() {
+                continue;
+            }
+
+            let src = src_file.as_ref().unwrap();
+            let meta = fs::metadata(src).expect("No access to file");
+
+            if meta.is_file() {
+                let pot_name = src.file_name();
+                if pot_name.is_none() {
+                    continue;
+                }
+                let name = pot_name.unwrap();
+                let target = target_path.join(name);
+
+                let res = copy_file(&src, &target);
+                if res.is_err() {
+                    println!("Error copying {}", src.display());
+                }
+            } else if meta.is_dir() {
+                let res = copy_folder(src, target_path);
+                if res.is_err() {
+                    eprintln!("Error copying {}", src.display());
+                }
+            }
+        }
+    }
+}
+
+fn delete(target: String) {
+    let target_files = glob(&target).expect("Invalid files !");
+
+    for file in target_files {
+        if file.is_err() {
+            continue;
+        }
+
+        let f = file.unwrap();
+        if f.is_dir() {
+            let res = fs::remove_dir_all(f.as_path());
+            if res.is_err() {
+                println!("Couldn't delete the folder {}", f.display());
+            }
+        } else {
+            let res = fs::remove_file(f.as_path());
+            if res.is_err() {
+                println!("Couldn't delete the file {}", f.display());
+            }
+        }
+    }
 }
 
 fn main() {
@@ -27,5 +226,61 @@ fn main() {
 
     match cli.command {
         Commands::List { dir, recursive } => list_dir(dir, recursive),
+        Commands::Copy { source, target } => copy(source, target),
+        Commands::Delete { target } => delete(target),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use crate::get_dir;
+
+    #[test]
+    fn test_get_dir_not_recursive() -> Result<(), String> {
+        fs::create_dir("./test").unwrap();
+        fs::File::create("./test/testA").unwrap();
+        fs::File::create("./test/testB").unwrap();
+        fs::File::create("./test/testC").unwrap();
+
+        let result = get_dir(&String::from("./test"), false, 0);
+
+        fs::remove_dir_all("./test").unwrap();
+
+        if result.len() != 3 {
+            return Err(format!(
+                "Result should be of length 3 but isn't\nResult:{:?}",
+                result
+            ));
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_dir_recursive() -> Result<(), String> {
+        fs::create_dir("./rectest").unwrap();
+        fs::create_dir("./rectest/test2").unwrap();
+
+        fs::File::create("./rectest/testA").unwrap();
+        fs::File::create("./rectest/testB").unwrap();
+        fs::File::create("./rectest/testC").unwrap();
+
+        fs::File::create("./rectest/test2/testD").unwrap();
+
+        let result = get_dir(&String::from("./rectest"), true, 0);
+
+        fs::remove_dir_all("./rectest").unwrap();
+
+        if result.len() != 5 {
+            return Err(format!(
+                "Result should be of length 5 but is {} \nResult:{:?}",
+                result.len(),
+                result
+            ));
+        }
+
+        Ok(())
     }
 }
